5a65123f8a0ab917958f8b0108f5d5fb7419635e
vikul rathod <vikul.rathod.net@gmail.com>
2023-02-28 10:02:49 +0530

    get set value in javascript demo

---
+++ JavascriptDemo/wwwroot/js/htmlelements.js
@@ -0,0 +1,50 @@
+﻿function getValues() {
+    var v1 = document.getElementById('txt1').value;
+    console.log('textbox value : ' + v1);
+
+    // v1 = document.getElementById('p1').innerText;
+    v1 = document.getElementById('p1').innerHTML;
+    console.log('paragraph text : ' + v1);
+
+    v1 = document.getElementById('div1').innerHTML;
+    console.log('div content : ' + v1);
+}
+
+function setValues() {
+    document.getElementById('txt2').value = 'VHAASH';
+    document.getElementById('p2').innerText = 'This is second paragraph';
+    document.getElementById('div2').innerHTML =
+        'Div 2 Content <br/>' +
+        '<a href="http://www.google.com">Visit Google</a>' +
+        '<span>Span Content</span>';
+
+    var txt = document.getElementById('txt2');
+    txt.style.backgroundColor = 'yellow';
+    txt.style.color = 'red';
+
+
+    document.getElementById('a1').setAttribute('href', 'http://www.facebook.com');
+    document.getElementById('a1').setAttribute('target', '_blank');
+
+}
+
+function readForm() {
+    var city = document.getElementById('city').value;
+    console.log('selected city : ' + city);
+
+    var rbMale = document.getElementById('rbMale');
+    var rbFemale = document.getElementById('rbFemale');
+
+    if (rbMale.checked) {
+        console.log('selected gender : ' + rbMale.value);
+    } else if (rbFemale.checked) {
+        console.log('selected gender : ' + rbFemale.value);
+    } else {
+        console.log('gender not selected');
+    }
+
+
+}
+
+var textareas = document.getElementsByTagName('textarea');
+textareas[1].style.backgroundColor = 'red';
---
+++ JavascriptDemo/wwwroot/js/imagegalary.js
@@ -0,0 +1,43 @@
+﻿var images = ['images/vihaan1.jpg', 'images/vihaan2.jpg', 'images/vihaan3.jpg',
+    'images/vihaan4.jpg', 'images/vihaan5.jpg'];
+
+var i = 0;
+function startSlideShow() {
+    // document.getElementById('photo').src = 'images/vihaan2.jpg';
+    //while (i < images.length) {
+    //    document.getElementById('photo').src = images[i];
+    //    i++;
+    //}
+
+    if (i == images.length) {
+        i = 0;
+    }
+
+    document.getElementById('photo').src = images[i];
+    i++;
+}
+
+function start() {
+    intervalId = setInterval(startSlideShow, 1000);
+}
+
+function stopSlideShow() {
+    clearInterval(intervalId);
+}
+
+function previousImage() {
+    document.getElementById('photo').src = images[i];
+    i--;
+    if (i <= 0) {
+        document.getElementById('prevButton').style.display = 'none';
+    }
+}
+
+function nextImage() {
+    // alert(i);
+    document.getElementById('photo').src = images[i];
+    i++;
+    if (i >= images.length) {
+        i = images.length - 1;
+    }
+}
