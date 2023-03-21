79788e25cbfb9e8731b8246283cf329e532aede3
vikul rathod <vikul.rathod.net@gmail.com>
2023-03-21 09:40:06 +0530

    doubts solving demos

---
+++ DummyApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DummyApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
